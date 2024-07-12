e7ad6b97936f4b260df53c7f778177a59d3332c5
Dimitris Ioannou <15361004+dioannou30@users.noreply.github.com>
2024-07-12 13:35:59 +0300

    Config Updates: 2024-07-12 13:35:59

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Personal LazyVim config
 ## LazyVim with some extra plugins for quality of life:
-* `"JoosepAlviste/palenightfall.nvim"` theme
+* `"baliestri/aura-theme"` theme
 * `Prettier` formatter using `conform/nvim`
 * `"DaikyXendo/nvim-material-icon"`
 * `EsLint` Or `BiomeJS` using `"mfussenegger/nvim-lint"` based on config discovery
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,65 +1,12 @@
 return {
-  "JoosepAlviste/palenightfall.nvim",
-  config = function()
-    require("palenightfall").setup({
-      transparent = false,
-      color_overrides = {},
-      highlight_overrides = {},
-    })
-  end,
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  {
+    "baliestri/aura-theme",
+    lazy = false,
+    priority = 1000,
+    config = function(plugin)
+      vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
+      vim.cmd([[colorscheme aura-dark]])
+    end,
+  },
 }
--- return {
---     "catppuccin/nvim",
---     name = "catppuccin",
---     config = function()
---         require("catppuccin").setup({
---             flavour = "auto", -- latte, frappe, macchiato, mocha
---             background = { -- :h background
---                 light = "latte",
---                 dark = "mocha"
---             },
---             transparent_background = false, -- disables setting the background color.
---             show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
---             term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
---             dim_inactive = {
---                 enabled = false, -- dims the background color of inactive window
---                 shade = "dark",
---                 percentage = 0.15 -- percentage of the shade to apply to the inactive window
---             },
---             no_italic = false, -- Force no italic
---             no_bold = false, -- Force no bold
---             no_underline = false, -- Force no underline
---             styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
---                 comments = {"italic"}, -- Change the style of comments
---                 conditionals = {"italic"},
---                 loops = {},
---                 functions = {},
---                 keywords = {},
---                 strings = {},
---                 variables = {},
---                 numbers = {},
---                 booleans = {},
---                 properties = {},
---                 types = {},
---                 operators = {}
---                 -- miscs = {}, -- Uncomment to turn off hard-coded styles
---             },
---             color_overrides = {},
---             custom_highlights = {},
---             default_integrations = true,
---             integrations = {
---                 cmp = true,
---                 gitsigns = true,
---                 nvimtree = true,
---                 treesitter = true,
---                 notify = false,
---                 mini = {
---                     enabled = true,
---                     indentscope_color = ""
---                 }
---                 -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
---             }
---         })
---         vim.cmd("colorscheme catppuccin")
---     end
--- }
